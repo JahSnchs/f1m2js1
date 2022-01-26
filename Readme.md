b00674fa3cebeec1fb4e57a6e62744489e52eb31
Student <Student@lt20320.media.local>
2022-01-26 11:01:01 +0100

    noice

--- Readme.md
+++ Readme.md
@@ -1,4 +1,10 @@
 # fase 1 Moodule 2 Museum Online
 ## Javascript
 
-[live view](http://127.0.0.1:5500/web/les1-background-color/index.html)
+Jah Sanches
+
+[Background Color](http://127.0.0.1:5500/web/les1-background-color/index.html)
+[groceries](http://127.0.0.1:5500/les2-boodschappen/index.html)
+[tentoonstelling](http://127.0.0.1:5500/les3-tentoonstelling/index.html)
+[seasons](http://127.0.0.1:5500/les4-seasons/index.html)
+[Silver Bullet](http://127.0.0.1:5500/les5-SilverBullet/index.html)
---
+++ les5-SilverBullet/script.js
@@ -0,0 +1,154 @@
+const myTitle = document.getElementById("myTitle");
+const myImage = document.getElementById("myImage");
+const myInput = document.getElementById("myInput");
+
+const buttonNoord = document.getElementById("buttonNoord");
+const buttonOost = document.getElementById("buttonOost");
+const buttonZuid = document.getElementById("buttonZuid");
+const buttonWest = document.getElementById("buttonWest");
+
+let directionButtons = {
+    "noord": document.getElementById("buttonNoord"),
+    "oost": document.getElementById("buttonOost"),
+    "zuid": document.getElementById("buttonZuid"),
+    "west": document.getElementById("buttonWest")
+}
+
+let current_index = 0;
+
+let locaties = [
+{
+    "title":"Plaats 0",
+    "img":"img/0.jpeg",
+    "directions": {
+        "zuid": 1
+    }
+},
+{
+    "title":"Plaats 1",
+    "img":"img/1.jpeg",
+    "directions": {
+        "noord": 0,
+        "oost": 5,
+        "zuid": 4,
+        "west": 2
+    }
+},
+{
+    "title":"Plaats 2",
+    "img":"img/2.jpeg",
+    "directions": {
+        "oost": 1,
+        "zuid": 3
+    }
+},
+{
+    "title":"Plaats 3",
+    "img":"img/3.jpeg",
+    "directions": {
+        "noord": 2
+    }
+},
+{
+    "title":"Plaats 4",
+    "img":"img/4.jpeg",
+    "directions": {
+        "noord": 1
+    }
+},
+{
+    "title":"Plaats 5",
+    "img":"img/5.jpeg",
+    "directions": {
+        "noord": 10,
+        "oost": 6,
+        "west": 1
+    }
+},
+{
+    "title":"Plaats 6",
+    "img":"img/6.jpeg",
+    "directions": {
+        "oost": 7,
+        "zuid": 11,
+        "west": 5
+    }
+},
+{
+    "title":"Plaats 7",
+    "img":"img/7.jpeg",
+    "directions": {
+        "noord": 9,
+        "zuid": 8,
+        "west": 6
+    }
+},
+{
+    "title":"Plaats 8",
+    "img":"img/8.jpeg",
+    "directions": {
+        "noord": 7
+    }
+},
+{
+    "title":"Plaats 9",
+    "img":"img/9.jpeg",
+    "directions": {
+        "zuid": 7,
+        "west": 10
+    }
+},
+{
+    "title":"Plaats 10",
+    "img":"img/10.jpeg",
+    "directions": {
+        "oost": 9,
+        "zuid": 5
+    }
+},
+{
+    "title":"Plaats 11",
+    "img":"img/11.jpeg",
+    "directions": {
+        "noord": 6
+    }
+}]
+
+function show(index) {
+
+    myTitle.innerHTML = locaties[index].title;
+    myImage.src = locaties[index].img;
+    current_index = index;
+
+    updateDirections();
+
+}
+
+function updateDirections() {
+    let possible = locaties[current_index].directions;
+
+    let possible_keys = Object.keys(possible);
+
+    let button_keys = Object.keys(directionButtons);
+
+    for(const key of button_keys) {
+        directionButtons[key].style.visibility = "hidden";
+    }
+    for(const key of possible_keys) {
+        directionButtons[key].style.visibility = "visible";
+    }
+}
+
+function getInput() {
+    console.log("input: " + myInput.value);
+
+    show(myInput.value);
+
+    myInput.value = "";
+    myInput.focus();
+}
+
+function goDirection(direction) {
+    let punt_index = locaties[current_index].directions[direction];
+    show(punt_index);
+}
